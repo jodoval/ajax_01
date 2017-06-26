e9eb6bed1faed5913cdb67be64aca8892ad918b4
jodoval <jodoval69@hotmail.om>
2017-06-26 02:47:43 +0200

    Actualización

--- cargar-tareas.php
+++ cargar-tareas.php
@@ -1,12 +1,12 @@
 <?php
-$conexion=new mysqli('localhost','root','','tareas_laravel');
-$resultado=$conexion->query ("SELECT * FROM tasks");
+$conexion=new mysqli('localhost','root','','gestion_tareas');
+$resultado=$conexion->query ("SELECT * FROM tareas");
 
 while ($fila=$resultado->fetch_assoc()){
   echo '<tr>';
   echo '<td>'.$fila['id'].'</td>';
   echo '<td>'.$fila['texto'].'</td>';
   echo '<td>'.$fila['estado'].'</td>';
-  echo '<td>'.$fila['usuario_id'].'</td>'; 
+  echo '<td>'.$fila['usuario_id'].'</td>';
   echo '</tr>';
 }
